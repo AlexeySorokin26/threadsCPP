986669f256784b70da576d5580a68c5451c0a904
Alexey Sorokin <asorokin@gremse-it.com>
2025-12-02 21:15:29 +0100

    first

---
+++ main.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class C {
+public:
+	int a;
+	C(int a) {
+		std::cout << "C constructor called" << std::endl;
+		std::cout << a << std::endl;
+	}
+};
+
+class CC {
+	C c = C(10);
+public:
+	CC(int a)  {
+		c.a = a;
+		std::cout << "CC constructor called" << std::endl;
+	}
+};
+
+int main() {
+	CC cc(0);
+}
